24805980032022fc51be74a6a0238e2b04cad546
ysd2445 <ysd2445@gmail.com>
2024-11-22 10:17:17 +0530

    some

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",  // Ensure the base path is set to root for correct asset paths
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'mui-vendor': ['@mui/material'],
-        },
-      },
-    },
-  },
+  plugins: [react()],
 });
